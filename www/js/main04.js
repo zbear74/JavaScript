cae8c25eebbae9ee50789e16072bdcaa430646b8
zbear74 <mzolotuhin@gmail.com>
2025-06-20 12:36:16 -0700

    add file 4 commit

---
+++ www/js/main04.js
@@ -0,0 +1,9 @@
+ //document.write("JS say Hello!!!");    
+/*
+console.log("Only Text: text in log");
+console.log("Date: ",Date()," ====>  Text in log");
+console.info("Info TEXT");
+console.warn("Warning: TEXT");
+console.error("Error: TEXT");
+*/
+console.log("Date: ",Date()," ====>  Text in log");
